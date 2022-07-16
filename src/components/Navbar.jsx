4a2220d581e3a4325f6cae9071e56cf018e068bc
André <andremessias.az@gmail.com>
2022-07-16 12:20:24 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./Navbar.css";
+
+function Navbar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(!search) return;
+
+    navigate(`/search?q=${search}`)
+    setSearch("")
+  }
+
+  return (
+    <nav className="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie />
+          MoviesOn
+        </Link>
+      </h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import {useState, useEffect} from 'react'
+import MovieCard from '../components/MovieCard'
+
+const moviesUrl = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
+import './MovieGrid.css'
+
+function Home() {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`
+
+    getTopRatedMovies(topRatedUrl);
+
+  }, [])
+
+  return (
+    <div className='container'>
+      <h2 className='title'>Melhores Filmes</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Home;
